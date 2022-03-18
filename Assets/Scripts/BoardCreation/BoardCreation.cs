0b13bec4b08a2afaf6e3a365852739b93be68105
OnistDerFalke <bazaanfil@gmail.com>
2022-03-18 23:04:59 +0100

    Ukończony skrypt rozmieszczający na mapie pola, drogi i miasta.

--- Assets/Scripts/BoardCreation/BoardCreation.cs
+++ Assets/Scripts/BoardCreation/BoardCreation.cs
@@ -9,6 +9,12 @@ public class BoardCreation : MonoBehaviour
     [Tooltip("Wysokość/położenie pola w pionie.")] [SerializeField]
     private float fieldHeight;
 
+    [Tooltip("Wymiary pojedynczej drogi.")] [SerializeField]
+    private Vector3 roadDimensions;
+
+    [Tooltip("Wymiary miasta.")] [SerializeField]
+    private Vector3 cityDimensions;
+
     [Tooltip("Środkowe pole na planszy.")] [SerializeField]
     private GameObject centerField;
 
@@ -17,12 +23,20 @@ public class BoardCreation : MonoBehaviour
 
     [Tooltip("Zewnętrzny pierścień pól na planszy.")] [SerializeField]
     private GameObject[] outerRing = new GameObject[12];
+    
+    [Tooltip("Drogi pomiędzy polami na planszy.")] [SerializeField]
+    private GameObject[] roads = new GameObject[72];
+    
+    [Tooltip("Miasta na planszy.")] [SerializeField]
+    private GameObject[] cities = new GameObject[54];
 
     private Vector3 fieldOffset;
 
     void Start()
     {
         SetTilesPositions();
+        SetRoadsPositions();
+        SetCitiesPositions();
     }
 
     private void SetTilesPositions()
@@ -54,5 +68,135 @@ public class BoardCreation : MonoBehaviour
         outerRing[9].gameObject.transform.position = new Vector3(-fieldOffset.x, fieldOffset.y, -3f * fieldOffset.z);
         outerRing[10].gameObject.transform.position = new Vector3(2 * fieldOffset.x, fieldOffset.y, 0f);
         outerRing[11].gameObject.transform.position = new Vector3(-2 * fieldOffset.x, fieldOffset.y, 0f);
+    }
+
+    private void SetRoadsPositions()
+    {
+        var fieldLength = (float)Math.Sqrt(3) * fieldRadius;
+        
+        /*Ustawienie rotacji*/
+        int clf = 0;
+        foreach (var road in roads)
+        {
+            clf++;
+            road.transform.localScale = roadDimensions;
+            if (clf <= 24) road.transform.rotation = Quaternion.Euler(0, 0, 0);
+            if (clf > 24 && clf <=48) road.transform.rotation = Quaternion.Euler(0, 60, 0);
+            if (clf > 48) road.transform.rotation = Quaternion.Euler(0, 120, 0);
+        }
+        
+        /*Pozycja dróg o podstawowej rotacji*/
+        roads[0].transform.position = new Vector3(0, 0, fieldRadius);
+        roads[1].transform.position = new Vector3(0, 0, 3*fieldRadius);
+        roads[2].transform.position = new Vector3(0, 0, 5*fieldRadius);
+        roads[3].transform.position = new Vector3(0, 0, -fieldRadius);
+        roads[4].transform.position = new Vector3(0, 0, -3*fieldRadius);
+        roads[5].transform.position = new Vector3(0, 0, -5*fieldRadius);
+        
+        roads[6].transform.position = new Vector3(1f*fieldLength, 0, 0);
+        roads[7].transform.position = new Vector3(1f*fieldLength, 0, 2*fieldRadius);
+        roads[8].transform.position = new Vector3(1f*fieldLength, 0, 4*fieldRadius);
+        roads[9].transform.position = new Vector3(1f*fieldLength, 0, -2*fieldRadius);
+        roads[10].transform.position = new Vector3(1f*fieldLength, 0, -4*fieldRadius);
+        
+        roads[11].transform.position = new Vector3(-1f*fieldLength, 0, 0);
+        roads[12].transform.position = new Vector3(-1f*fieldLength, 0, 2*fieldRadius);
+        roads[13].transform.position = new Vector3(-1f*fieldLength, 0, 4*fieldRadius);
+        roads[14].transform.position = new Vector3(-1f*fieldLength, 0, -2*fieldRadius);
+        roads[15].transform.position = new Vector3(-1f*fieldLength, 0, -4*fieldRadius);
+        
+        roads[16].transform.position = new Vector3(2f*fieldLength, 0, fieldRadius);
+        roads[17].transform.position = new Vector3(2f*fieldLength, 0, 3*fieldRadius);
+        roads[18].transform.position = new Vector3(2f*fieldLength, 0, -fieldRadius);
+        roads[19].transform.position = new Vector3(2f*fieldLength, 0, -3*fieldRadius);
+        
+        roads[20].transform.position = new Vector3(-2f*fieldLength, 0, fieldRadius);
+        roads[21].transform.position = new Vector3(-2f*fieldLength, 0, 3*fieldRadius);
+        roads[22].transform.position = new Vector3(-2f*fieldLength, 0, -fieldRadius);
+        roads[23].transform.position = new Vector3(-2f*fieldLength, 0, -3*fieldRadius);
+
+        /*Przesunięcie dróg po rotacji*/
+        clf = 0;
+        var firstOffset = new Vector3(-fieldLength/2, 0 ,fieldRadius/2);
+        var secondOffset = new Vector3(0, 0 , -fieldRadius);
+        foreach (var road in roads)
+        {
+            clf++;
+            if (clf > 24 && clf <= 48)
+                road.transform.position = roads[clf-25].transform.position + firstOffset;
+            if (clf > 48) 
+                road.transform.position = roads[clf-25].transform.position + secondOffset;
+        }
+        
+        /*Podmiana dróg spoza zakresu*/
+        
+        //dla rotacji 60 stopni
+        roads[26].transform.position = new Vector3(2.5f*fieldLength, 0, 0.5f*fieldRadius);
+        roads[37].transform.position = new Vector3(2.5f*fieldLength, 0, 2.5f*fieldRadius);
+        roads[45].transform.position = new Vector3(2.5f*fieldLength, 0, -1.5f*fieldRadius);
+        //dla rotacji 120 stopni
+        roads[53].transform.position = new Vector3(2.5f*fieldLength, 0, -0.5f*fieldRadius);
+        roads[63].transform.position = new Vector3(2.5f*fieldLength, 0, -2.5f*fieldRadius);
+        roads[71].transform.position = new Vector3(2.5f*fieldLength, 0, 1.5f*fieldRadius);
+    }
+
+    private void SetCitiesPositions()
+    {
+        var fieldLength = centerField.transform.localScale.x / 2;
+
+        /*Początkowa pozycja i rozmiar*/
+        foreach (var city in cities)
+        {
+            city.transform.localScale = cityDimensions;
+            city.transform.position = new Vector3(0, 0, 0);
+        }
+        
+        /*Miasta parzyste*/
+        cities[0].transform.position = new Vector3(fieldLength, 0, 0);
+        cities[1].transform.position = new Vector3(-fieldLength, 0, 0);
+        cities[2].transform.position = new Vector3(2 * fieldLength, 0, 0);
+        cities[3].transform.position = new Vector3(-2 *fieldLength, 0, 0);
+        cities[4].transform.position = new Vector3(4 * fieldLength, 0, 0);
+        cities[5].transform.position = new Vector3(-4 * fieldLength, 0, 0);
+        
+        for (var i = 0; i < 6; i++)
+        {
+            cities[i + 6].transform.position = cities[i].transform.position 
+                                               + new Vector3(0, 0, 2 * fieldRadius);
+            cities[i + 12].transform.position = cities[i].transform.position 
+                                               + new Vector3(0, 0, -2 * fieldRadius);
+            if (i < 4)
+            {
+                cities[i + 18].transform.position = cities[i].transform.position 
+                                                    + new Vector3(0, 0, 4 * fieldRadius);
+                cities[i + 22].transform.position = cities[i].transform.position 
+                                                    + new Vector3(0, 0, -4 * fieldRadius);
+            }
+        }
+        
+        /*Miasta nieparzyste*/
+        cities[26].transform.position = new Vector3(0.5f*fieldLength, 0, fieldRadius);
+        cities[27].transform.position = new Vector3(-0.5f*fieldLength, 0, fieldRadius);
+        cities[28].transform.position = new Vector3(2.5f*fieldLength, 0, fieldRadius);
+        cities[29].transform.position = new Vector3(-2.5f*fieldLength, 0, fieldRadius);
+        cities[30].transform.position = new Vector3(3.5f*fieldLength, 0, fieldRadius);
+        cities[31].transform.position = new Vector3(-3.5f*fieldLength, 0, fieldRadius);
+
+        for (var i = 26; i < 32; i++)
+        {
+            cities[i + 6].transform.position = cities[i].transform.position 
+                                               + new Vector3(0, 0, -2 * fieldRadius);
+            cities[i + 12].transform.position = cities[i].transform.position 
+                                               + new Vector3(0, 0, 2 * fieldRadius);
+            cities[i + 18].transform.position = cities[i].transform.position 
+                                               + new Vector3(0, 0, -4 * fieldRadius);
+            if (i < 28)
+            {
+                cities[i + 24].transform.position = cities[i].transform.position 
+                                                   + new Vector3(0, 0, 4 * fieldRadius);
+                cities[i + 26].transform.position = cities[i].transform.position 
+                                                   + new Vector3(0, 0, -6 * fieldRadius);
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Catan
 Projekt inżynierski dyplomowy.
+
+Wprowadzone aktualizacje:
+
+<b>18 Marzec 2022</b>
+* Dodano podstawowe menu główne gry.
+* Dodano skrypt umieszczający elementy takie jak drogi, miasta, czy pola na mapie gry.
+* Dodano animacje ruchu kamerą w menu głównym.
+* Wprowadzono płynne przejście kamery z menu głównego do właściwej gry.
+
