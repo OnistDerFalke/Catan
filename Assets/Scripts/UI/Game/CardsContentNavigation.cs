a715dde2a45588c42d2ba21d452b11d551252829
Onist Der Falke <75864407+OnistDerFalke@users.noreply.github.com>
2022-06-24 06:48:27 +0200

    Podpięcie metod do użycia karty i blokowanie przycisku użycia karty

--- Assets/Scripts/UI/Game/CardsContentNavigation.cs
+++ Assets/Scripts/UI/Game/CardsContentNavigation.cs
@@ -16,6 +16,32 @@ namespace UI.Game
         [Tooltip("Monopol Card Number")]
         [SerializeField] private Text monopolCardNumber;
         
+        [Header("Available Cards Buttons")][Space(5)]
+        [Tooltip("Knight Card Button")]
+        [SerializeField] private Button knightCardButton;
+        [Tooltip("Road Build Card Button")]
+        [SerializeField] private Button roadBuildCardButton;
+        [Tooltip("Invention Card Button")]
+        [SerializeField] private Button inventionCardButton;
+        [Tooltip("Monopol Card Button")]
+        [SerializeField] private Button monopolCardButton;
+
+        [Header("Use Card Button")][Space(5)]
+        [Tooltip("Use Card Button")]
+        [SerializeField] private Button useCardButton;
+        
+        private Player.Cards.CardType cardChosen;
+        void Start()
+        {
+            useCardButton.enabled = false;
+            
+            knightCardButton.onClick.AddListener(() => { ChooseCardButton(Player.Cards.CardType.Knight);});
+            roadBuildCardButton.onClick.AddListener(() => { ChooseCardButton(Player.Cards.CardType.RoadBuild);});
+            inventionCardButton.onClick.AddListener(() => { ChooseCardButton(Player.Cards.CardType.Invention);});
+            monopolCardButton.onClick.AddListener(() => { ChooseCardButton(Player.Cards.CardType.Monopol);});
+            useCardButton.onClick.AddListener(OnCardUseButtonClick);
+        }
+        
         void Update()
         {
             UpdateCardsAvailable();
@@ -53,6 +79,45 @@ namespace UI.Game
                 monopolCardNumber.color = Color.red;
             else
                 monopolCardNumber.color = Color.white;
+        }
+
+        private void ChooseCardButton(Player.Cards.CardType type)
+        {
+            cardChosen = type;
+            
+            //Destiny: if card is blocked or it's not available, it cannot be used
+            var currentPlayerCards = GameManager.Players[GameManager.CurrentPlayer].properties.cards;
+            var currentPlayerBlockedCards = GameManager.Players[GameManager.CurrentPlayer].properties.cards.CheckIfMarkAsBlocked();
+            if (currentPlayerBlockedCards[type] || currentPlayerCards.GetCardNumber(type)<=0) return;
+            
+            //Destiny: if card not blocked, it is now chosen and use card button is getting unlocked
+            useCardButton.enabled = true;
+        }
+
+        /// <summary>
+        /// Handles click on use button of the card
+        /// </summary>
+        private void OnCardUseButtonClick()
+        {
+            //Destiny: Blocks the use button after clicking it
+            useCardButton.enabled = false;
+            
+            //Destiny: Handles card use event of card chosen
+            switch (cardChosen)
+            {
+                case Player.Cards.CardType.Knight:
+                    //TODO: Card use handle (dont forget to remove card from player)
+                    break;
+                case Player.Cards.CardType.RoadBuild:
+                    //TODO: Card use handle (dont forget to remove card from player)
+                    break;
+                case Player.Cards.CardType.Invention:
+                    //TODO: Card use handle (dont forget to remove card from player)
+                    break;
+                case Player.Cards.CardType.Monopol:
+                    //TODO: Card use handle (dont forget to remove card from player)
+                    break;
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Catan
 Projekt inżynierski dyplomowy.
 
-Wprowadzone aktualizacje:
+<b>OnistDerFalke DevLog</b>:
 
-<b>18 Marzec 2022</b>
-* Dodano podstawowe menu główne gry.
-* Dodano skrypt umieszczający elementy takie jak drogi, miasta, czy pola na mapie gry.
-* Dodano animacje ruchu kamerą w menu głównym.
-* Wprowadzono płynne przejście kamery z menu głównego do właściwej gry.
+<b>24 Czerwiec 2022</b>
+* Dla <span style="color: purple"><i>issue #36</i></span> dodałem funkcjonalność wyboru karty poprzez kliknięcie na nią, a następnie przycisku użycia karty.
+Wszystkie zmiany zachodzą w skrypcie CardsContentNavigation. Tam też znajduje się metoda <span style="color: green"><b><i>OnCardUseButtonClick</b></i></span>, w której switch pozwala
+na wywołanie danego eventu w zależności od wybranej karty.
+* Poblokowałem też możliwość wciśnięcia przycisku użycia karty w sytuacji gdy jest ona oznaczona jako zablokowana lub gdy gracz 
+posiada mniej niż jedną kartę. Przycisk jest blokowany na starcie, po wybraniu karty - karta jest przypisywana do wybranej, ale przycisk
+odblokuje się dopiero gdy zostaną spełnione powyższe warunki. Cały ten if siedzi w metodzie <span style="color: green"><b><i>ChooseCardButton</b></i></span> i tam jest ładnie to opisane
+jeszcze w komentarzach.
+
+
 
