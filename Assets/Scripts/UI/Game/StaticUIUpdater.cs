c5177bd5606b9cf7c84125dbccfa795aeef7a44c
Onist Der Falke <75864407+OnistDerFalke@users.noreply.github.com>
2022-07-12 09:52:56 +0200

    StaticUIUpdater refactor

--- Assets/Scripts/UI/Game/StaticUIUpdater.cs
+++ Assets/Scripts/UI/Game/StaticUIUpdater.cs
@@ -12,32 +12,55 @@ namespace UI.Game
     /// </summary>
     public class StaticUIUpdater : MonoBehaviour
     {
+        //Destiny: Current player info
         [Header("Current Player UI")][Space(5)]
-        [Tooltip("Current player color image")] [SerializeField]
-        private Image playerColorImage;
-        [Tooltip("Current player name text")] [SerializeField]
-        private Text playerNameText;
+        [Tooltip("Current player color image")] [SerializeField] private Image playerColorImage;
+        [Tooltip("Current player name text")] [SerializeField] private Text playerNameText;
         
+        //Destiny: Selected element with additional info
         [Header("Selected Element UI")][Space(5)]
-        [Tooltip("Selected element owner text")] [SerializeField]
-        private Text selectedElementOwner;
-        [Tooltip("Selected element name text")] [SerializeField]
-        private Text selectedElementName;
+        [Tooltip("Selected element owner text")] [SerializeField] private Text selectedElementAdditionalInfo;
+        [Tooltip("Selected element name text")] [SerializeField] private Text selectedElementName;
 
+        //Destiny: Current player's resources info
         [Header("Resources")][Space(5)]
-        [Tooltip("Wood resource text")] [SerializeField]
-        private Text woodResourceText;
-        [Tooltip("Clay resource text")] [SerializeField]
-        private Text clayResourceText;
-        [Tooltip("Wool resource text")] [SerializeField]
-        private Text woolResourceText;
-        [Tooltip("Iron resource text")] [SerializeField]
-        private Text ironResourceText;
-        [Tooltip("Wheat resource text")] [SerializeField]
-        private Text wheatResourceText;
+        [Tooltip("Wood resource text")] [SerializeField] private Text woodResourceText;
+        [Tooltip("Clay resource text")] [SerializeField] private Text clayResourceText;
+        [Tooltip("Wool resource text")] [SerializeField] private Text woolResourceText;
+        [Tooltip("Iron resource text")] [SerializeField] private Text ironResourceText;
+        [Tooltip("Wheat resource text")] [SerializeField] private Text wheatResourceText;
+        
+        //Destiny: Board elements names shown in UI
+        [Header("Board elements names")][Space(5)]
+        [Tooltip("Empty junction name")] [SerializeField] private string junctionEmptyName;
+        [Tooltip("Village junction name")] [SerializeField] private string junctionVillageName;
+        [Tooltip("City junction name")] [SerializeField] private string junctionCityName;
+        [Tooltip("Path name")] [SerializeField] private string pathName;
+        [Tooltip("Clay field name")] [SerializeField] private string clayFieldName;
+        [Tooltip("Desert field name")] [SerializeField] private string desertFieldName;
+        [Tooltip("Field field name")] [SerializeField] private string fieldFieldName;
+        [Tooltip("Forest field name")] [SerializeField] private string forestFieldName;
+        [Tooltip("Mountains field name")] [SerializeField] private string mountainsFieldName;
+        [Tooltip("Pasture field name")] [SerializeField] private string pastureFieldName;
+
+        //Destiny: When we point a field, it gives info which resource is supplied by this field
+        [Header("Supplied resources names")][Space(5)]
+        [Tooltip("Supplied prefix")] [SerializeField] private string suppliedPrefix;
+        [Tooltip("Wood")] [SerializeField] private string suppliedWood;
+        [Tooltip("Wheat")] [SerializeField] private string suppliedWheat;
+        [Tooltip("Clay")] [SerializeField] private string suppliedClay;
+        [Tooltip("Iron")] [SerializeField] private string suppliedIron;
+        [Tooltip("Wool")] [SerializeField] private string suppliedWool;
+        [Tooltip("Supplies nothing")] [SerializeField] private string suppliedNothing;
+        
+        [Header("Element owner")][Space(5)]
+        [Tooltip("Owner prefix")] [SerializeField] private string ownerPrefix;
+        [Tooltip("No owner")] [SerializeField] private string noOwner;
+
         
         void Update()
         {
+            //Destiny: Updates all info in UI
             UpdateSelectedElement();
             UpdateCurrentPlayer();
             UpdateResources();
@@ -61,7 +84,7 @@ namespace UI.Game
         }
         
         /// <summary>
-        /// Updates color of the current player on the UI
+        /// Updates color and name of the current player on the UI
         /// </summary>
         private void UpdateCurrentPlayer()
         {
@@ -83,8 +106,8 @@ namespace UI.Game
         {
             if (GameManager.Selected.Pointed == null)
             {
-                selectedElementName.text = "Nie wybrano";
-                selectedElementOwner.text = "Brak";
+                selectedElementName.text = "";
+                selectedElementAdditionalInfo.text = "";
                 return;
             }
             
@@ -94,12 +117,12 @@ namespace UI.Game
                 var element = (FieldElement) GameManager.Selected.Pointed;
                 selectedElementName.text = element.type switch
                 {
-                    FieldElement.FieldType.Forest => "Las",
-                    FieldElement.FieldType.Pasture => "Pastwisko",
-                    FieldElement.FieldType.Field => "Pole uprawne",
-                    FieldElement.FieldType.Hills => "Złoże miedzi",
-                    FieldElement.FieldType.Mountains => "Góry",
-                    FieldElement.FieldType.Desert => "Pustynia",
+                    FieldElement.FieldType.Forest => forestFieldName,
+                    FieldElement.FieldType.Pasture => pastureFieldName,
+                    FieldElement.FieldType.Field => fieldFieldName,
+                    FieldElement.FieldType.Hills => clayFieldName,
+                    FieldElement.FieldType.Mountains => mountainsFieldName,
+                    FieldElement.FieldType.Desert => desertFieldName,
                     _ => selectedElementName.text
                 };
             }
@@ -108,50 +131,59 @@ namespace UI.Game
                 var element = (JunctionElement) GameManager.Selected.Pointed;
                 selectedElementName.text = element.type switch
                 {
-                    JunctionElement.JunctionType.None => "Puste skrzyżowanie",
-                    JunctionElement.JunctionType.Village => "Wioska",
-                    JunctionElement.JunctionType.City => "Miasto",
+                    JunctionElement.JunctionType.None => junctionEmptyName,
+                    JunctionElement.JunctionType.Village => junctionVillageName,
+                    JunctionElement.JunctionType.City => junctionCityName,
                     _ => selectedElementName.text
                 };
             }
             else if (GameManager.Selected.Pointed as PathElement != null)
             {
-                selectedElementName.text = "Droga";
+                selectedElementName.text = pathName;
             }
             
             //Destiny: Setting additional info
-            selectedElementOwner.text = "Brak";
+            selectedElementAdditionalInfo.text = "";
             foreach (var player in GameManager.Players)
             {
+                //Destiny: For field additional info is which resource it supplies
                 if (GameManager.Selected.Pointed as FieldElement != null)
                 {
                     var element = (FieldElement) GameManager.Selected.Pointed;
-                    selectedElementOwner.text = element.type switch
+                    selectedElementAdditionalInfo.text = element.type switch
                     {
-                        FieldElement.FieldType.Forest => "Dostarcza: Drewno",
-                        FieldElement.FieldType.Pasture => "Dostarcza: Wełna",
-                        FieldElement.FieldType.Field => "Dostarcza: Zboże",
-                        FieldElement.FieldType.Hills => "Dostarcza: Glina",
-                        FieldElement.FieldType.Mountains => "Dostarcza: Żelazo",
-                        FieldElement.FieldType.Desert => "Nie dostarcza surowców",
-                        _ => selectedElementOwner.text
+                        FieldElement.FieldType.Forest => suppliedPrefix + suppliedWood,
+                        FieldElement.FieldType.Pasture => suppliedPrefix + suppliedWool,
+                        FieldElement.FieldType.Field => suppliedPrefix + suppliedWheat,
+                        FieldElement.FieldType.Hills => suppliedPrefix + suppliedClay,
+                        FieldElement.FieldType.Mountains => suppliedPrefix + suppliedIron,
+                        FieldElement.FieldType.Desert => suppliedNothing,
+                        _ => selectedElementAdditionalInfo.text
                     };
                     break;
                 }
                 
+                //Destiny: For path and junction additional info is an owner
                 if (GameManager.Selected.Pointed as JunctionElement != null)
                 {
                     var element = (JunctionElement) GameManager.Selected.Pointed;
-                    if (!player.OwnsBuilding(element.id)) continue;
-                    selectedElementOwner.text = player.name;
+                    if (!player.OwnsBuilding(element.id))
+                    {
+                        selectedElementAdditionalInfo.text = noOwner;
+                        continue;
+                    }
+                    selectedElementAdditionalInfo.text = ownerPrefix + player.name;
                     break;
                 }
-                
                 if (GameManager.Selected.Pointed as PathElement != null)
                 {
                     var element = (PathElement) GameManager.Selected.Pointed;
-                    if (!player.OwnsPath(element.id)) continue;
-                    selectedElementOwner.text = player.name;
+                    if (!player.OwnsPath(element.id))
+                    {
+                        selectedElementAdditionalInfo.text = noOwner;
+                        continue;
+                    }
+                    selectedElementAdditionalInfo.text = ownerPrefix + player.name;
                     break;
                 }
             }
